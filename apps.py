e1113f392c7ca8d0c47928086659ea2b93fa6c9e
Aibek <101047236+t4iler@users.noreply.github.com>
2022-05-23 12:53:54 +0600

    Create apps.py

---
+++ apps.py
@@ -0,0 +1,11 @@
+import os
+#This is a software library designed for file management.
+def word():
+    os.startfile('C:/Program Files/Microsoft Office/root/Office16/WINWORD.EXE')   #link, where is my app located
+
+def powerpoint():
+    os.startfile('C:/Program Files/Microsoft Office/root/Office16/POWERPNT.EXE')   #link, where is my app located
+
+
+# assist = word()
+# assist = powerpoint()
